b585530882660b05cb311957e63ed7b69e225e70
asaf <asafmohammad5@gmail.com>
2019-12-30 19:36:51 -0500

    Add swimmer animation, up and down functionality

--- src/game.js
+++ src/game.js
@@ -5,6 +5,7 @@ export default class SwimBubble{
   constructor(canvas) {
     this.ctx = canvas.getContext("2d");
     this.dimensions = { width: canvas.width, height: canvas.height };
+    canvas.addEventListener('click', this.click.bind(this));
     this.begin();
   }
 
@@ -17,10 +18,20 @@ export default class SwimBubble{
   frame () {
     this.level.frame(this.ctx);
     this.swimmer.frame(this.ctx);
+    if (this.playing) {
+      requestAnimationFrame(this.frame.bind(this));
+    }
   }
 
   start() {
-    this.running = true;
+    this.playing = true;
     this.frame();
   };
+
+  click () {
+    if (!this.playing) {
+      this.start();
+    }
+    this.swimmer.swim();
+  }
 }
