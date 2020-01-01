ac2c0c43d7aabe46ed99043271f9f9f36d1a1ef6
asaf <asafmohammad5@gmail.com>
2020-01-01 18:00:37 -0500

    Add small air bubbles to game

--- src/level.js
+++ src/level.js
@@ -13,7 +13,12 @@ export default class Level {
     this.bubbles = [ { x: (CONSTANTS.BUBBLE_DISTANCE * 4), y: this.bubblePosition() },
       { x: (CONSTANTS.BUBBLE_DISTANCE * 5), y: this.bubblePosition() },
       { x: (CONSTANTS.BUBBLE_DISTANCE * 6), y: this.bubblePosition() } ];
-  }
+    this.smallBubbles = [ {x: (this.bubblePositionX()), y: this.dimensions.height + this.bubblePositionY() }, 
+      { x: (this.bubblePositionX()), y: this.dimensions.height + this.bubblePositionY()}, 
+      { x: (this.bubblePositionX()), y: this.dimensions.height + this.bubblePositionY() }, 
+      { x: (this.bubblePositionX()), y: this.dimensions.height + this.bubblePositionY()},
+      { x: (this.bubblePositionX()), y: this.dimensions.height + this.bubblePositionY() } ];
+  };
 
   gotBubble(bounds) {
     let bubble = this.bubbles[0];
@@ -114,13 +119,14 @@ export default class Level {
   }
 
   frame(ctx) {
-   
     this.moveBubble();
+    this.moveBubbles()
     this.drawBackground(ctx);
     ctx.fillStyle = "white";
     ctx.font = "bold 22px Arial";
     ctx.fillText(`Score: ${this.score}`, (this.dimensions.width / 1.15), (this.dimensions.height / 20));
     this.drawBubble(ctx);
+    this.drawSmallBubble(ctx);
    
   }
 
@@ -134,6 +140,19 @@ export default class Level {
         this.bubbles.push(endBubble);
       }
       bubble.x += CONSTANTS.BUBBLE_SPEED;
+    }
+  };
+
+  moveBubbles() {
+    for (let i = 0; i < this.smallBubbles.length; i++) {
+      const bubble = this.smallBubbles[i];
+      if (bubble.y < 0 - 8) {
+        let endBubble = this.smallBubbles.shift();
+        endBubble.y = this.dimensions.height + this.bubblePositionY();
+        endBubble.x = this.bubblePositionX();
+        this.smallBubbles.push(endBubble);
+      }
+      bubble.y += CONSTANTS.BUBBLE_SPEED * 2.5;
     }
   };
 
@@ -151,10 +170,40 @@ export default class Level {
     }
   };
 
+  drawSmallBubble(ctx) {
+    for (let i = 0; i < this.smallBubbles.length; i++) {
+      const bubble = this.smallBubbles[i];
+      if (bubble.y < this.dimensions.height) {
+        ctx.beginPath()
+        ctx.arc(bubble.x, bubble.y, 8, 0, 2 * Math.PI);
+        ctx.fillStyle = `rgba(0, 0, 0, 0)`;
+        ctx.fill();
+        ctx.strokeStyle = `#FFFFFF`;
+        ctx.stroke();
+      }
+    }
+  };
+
   bubblePosition() {
     let num = Math.floor(Math.random() * 800);
     while (num < 100 || num > 702) {
       num = Math.floor(Math.random() * 800);
+    }
+    return num;
+  }
+
+  bubblePositionX() {
+    let num = Math.floor(Math.random() * 1000);
+    while (num < 25 || num > 975) {
+      num = Math.floor(Math.random() * 1000);
+    }
+    return num;
+  }
+
+  bubblePositionY() {
+    let num = Math.floor(Math.random() * 200);
+    while (num < 25 || num > 180) {
+      num = Math.floor(Math.random() * 200);
     }
     return num;
   }
