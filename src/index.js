d534a05c0a1c7ba7d691bf3eef6d4a982b34e834
asaf <asafmohammad5@gmail.com>
2020-02-01 20:05:03 -0600

    Add media queries for larger and smaller devices

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import SwimBubble from './game';
 
 document.addEventListener("DOMContentLoaded", function () {
-  const canvas = document.getElementById('game');
+  let canvas = document.getElementById('game');
   new SwimBubble(canvas);
 });
